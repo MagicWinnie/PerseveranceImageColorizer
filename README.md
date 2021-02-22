f076ffbf030140c1f8d58b5aba4233814f09a5fd
MagicWinnie <magicwinnie@yandex.ru>
2021-02-22 22:10:41 +0700

    Added manual mode

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # Parsing and combining channels of Perseverance Mars Rover
 ## Disclaimer
 - ### As the same images but with different channels do not have similar filenames, the script uses scikit-image's `compare_ssim` which might not correctly find the correct images to merge with.  
-- ### As NASA's API does not support Perseverance, the script requires `selenium` and `geckodriver` for parsing. If you have downloaded the images by yourself then just create `raw_image` folder and run `combine.py`  
+- ### As NASA's API does not support Perseverance, the script requires `selenium` and `geckodriver` for parsing. If you have downloaded the images by yourself then just create `raw_image` folder and run `python3 combine.py`  
+- ### Or just run `python3 combine.py <json_file.json>` if you have a json file with following pattern with a specific order of files  
+    -   ```json
+        {
+            "0": ["red_channel_file.png", "green_channel_file.png", "blue_channel_file.png"],
+            "1" : ...,
+            ...
+            "123" : ["red_channel_file.png", "green_channel_file.png", "blue_channel_file.png"]
+        }
+        ```
 ## Requirements  
 `pip3 install -r requirements.txt`  
 ## How to run  
--- combine.py
+++ combine.py
@@ -11,6 +11,7 @@ from skimage.measure import compare_ssim
 import cv2
 import json
 import os
+import sys
 
 def pretty(d, indent = 0):
     for key, value in d.items():
@@ -82,80 +83,88 @@ imgs = dict()
 
 m = max(len(R), len(G), len(B), len(E), len(M))
 
-print(len(R), len(G), len(B), len(E), len(M))
+# print(len(R), len(G), len(B), len(E), len(M))
 
-for f in FILES:
-    key = f.split('_')[0][:-1]
-    img = cv2.imread(os.path.join(SAVE_PATH, f), 0)
-    
-    if key not in imgs:
-        imgs[key] = {}
-    if str(img.shape) not in imgs[key]:
-        imgs[key][str(img.shape)] = {}
-    if f.split('_')[1] not in imgs[key][str(img.shape)]:
-        imgs[key][str(img.shape)][f.split('_')[1]] = {'R': [], 'G': [], 'B': [], 'E': [], 'M': []}
+argv = sys.argv
+if len(argv) > 1 and argv[1].split('.')[-1] == 'json':
+    with open(argv[1], 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    for key in data:
+        if len(data[key]) != 3:
+            print("PASSING:", key)
+            continue
+        a = cv2.imread(os.path.join(SAVE_PATH, data[key][0]), 0)
+        b = cv2.imread(os.path.join(SAVE_PATH, data[key][1]), 0)
+        c = cv2.imread(os.path.join(SAVE_PATH, data[key][2]), 0)
+        assert a.shape == b.shape == c.shape
+        cv2.imwrite(os.path.join(IMG_PATH, data[key][0]), cv2.merge((c, b, a)))
+else:
+    for f in FILES:
+        key = f.split('_')[0][:-1]
+        img = cv2.imread(os.path.join(SAVE_PATH, f), 0)
+        
+        if key not in imgs:
+            imgs[key] = {}
+        if str(img.shape) not in imgs[key]:
+            imgs[key][str(img.shape)] = {}
+        if f.split('_')[1] not in imgs[key][str(img.shape)]:
+            imgs[key][str(img.shape)][f.split('_')[1]] = {'R': [], 'G': [], 'B': [], 'E': [], 'M': []}
 
-    imgs[key][str(img.shape)][f.split('_')[1]][f.split('_')[0][-1]].append(f)
-    # imgs[key][str(img.shape)][f.split('_')[0][-1]][-1]['img'] = img
-    imgs[key][str(img.shape)][f.split('_')[1]][f.split('_')[0][-1]].sort()
+        imgs[key][str(img.shape)][f.split('_')[1]][f.split('_')[0][-1]].append(f)
+        # imgs[key][str(img.shape)][f.split('_')[0][-1]][-1]['img'] = img
+        imgs[key][str(img.shape)][f.split('_')[1]][f.split('_')[0][-1]].sort()
 
-with open('imgs.json', 'w') as f:
-    json.dump(imgs, f, indent=4)
+    for cam in imgs:
+        for size in imgs[cam]:
+            temp_size = eval(size)
+            if temp_size[0] < 100 and temp_size[1] < 100: continue
+            for sol in imgs[cam][size]:
+                temp = imgs[cam][size][sol]
+                if len(temp["R"]) == 0 or len(temp["G"]) == 0 or len(temp["B"]) == 0:
+                    continue
+                min_len_key = min(temp.items(), key=lambda x: len(x))[0]
 
-visited = set()
-
-for cam in imgs:
-    for size in imgs[cam]:
-        temp_size = eval(size)
-        if temp_size[0] < 100 and temp_size[1] < 100: continue
-        for sol in imgs[cam][size]:
-            temp = imgs[cam][size][sol]
-            if len(temp["R"]) == 0 or len(temp["G"]) == 0 or len(temp["B"]) == 0:
-                continue
-            min_len_key = min(temp.items(), key=lambda x: len(x))[0]
-
-            if min_len_key == "R":
-                others = ["G", "B"]
-            elif min_len_key == "G":
-                others = ["R", "B"]
-            else:
-                others = ["R", "G"]
-            
-            for i in temp[min_len_key]:
-                visited.add(i)
-                orig = cv2.imread(os.path.join(SAVE_PATH, i), 0)
-                others_images = {}
-                for key in others:
-                    values = []
-                    imgs_values = []
-                    for other in temp[key]:
-                        new = cv2.imread(os.path.join(SAVE_PATH, other), 0)
-                        (score, diff) = compare_ssim(orig, new, full=True)
-                        diff = (diff * 255).astype("uint8")
-                        values.append(score)
-                        imgs_values.append(new)
-                    if max(values) < 0.8:
-                        continue
-                    others_images[key] = imgs_values[values.index(max(values))]
-                if len(others_images.keys()) > 0:
-                    # cv2.imshow('orig', image_resize(orig, width=300))
-                    # cv2.imshow('other1', image_resize(others_images[others[0]], width=300))
-                    # cv2.imshow('other2', image_resize(others_images[others[1]], width=300))
-                    # orig = np.uint8(orig)
-                    # others_images[others[0]] = np.uint8(others_images[others[0]])
-                    # others_images[others[1]] = np.uint8(others_images[others[1]])
-                    if min_len_key == "R" and others[0] == "G":
-                        image = cv2.merge((others_images[others[1]], others_images[others[0]], orig))
-                    elif min_len_key == "R" and others[0] == "B":
-                        image = cv2.merge((others_images[others[0]], others_images[others[1]], orig))
-                    elif min_len_key == "G" and others[0] == "R":
-                        image = cv2.merge((others_images[others[1]], orig, others_images[others[0]]))
-                    elif min_len_key == "G" and others[0] == "B":
-                        image = cv2.merge((others_images[others[0]], orig, others_images[others[1]]))
-                    elif min_len_key == "B" and others[0] == "R":
-                        image = cv2.merge((orig, others_images[others[1]], others_images[others[0]]))
-                    elif min_len_key == "B" and others[0] == "G":
-                        image = cv2.merge((orig, others_images[others[0]], others_images[others[1]]))
-                    cv2.imwrite(os.path.join(IMG_PATH, i), image)
-                    # cv2.waitKey(0)
+                if min_len_key == "R":
+                    others = ["G", "B"]
+                elif min_len_key == "G":
+                    others = ["R", "B"]
+                else:
+                    others = ["R", "G"]
                 
+                for i in temp[min_len_key]:
+                    orig = cv2.imread(os.path.join(SAVE_PATH, i), 0)
+                    others_images = {}
+                    for key in others:
+                        values = []
+                        imgs_values = []
+                        for other in temp[key]:
+                            new = cv2.imread(os.path.join(SAVE_PATH, other), 0)
+                            (score, diff) = compare_ssim(orig, new, full=True)
+                            diff = (diff * 255).astype("uint8")
+                            values.append(score)
+                            imgs_values.append(new)
+                        if max(values) < 0.8:
+                            continue
+                        others_images[key] = imgs_values[values.index(max(values))]
+                    if len(others_images.keys()) > 0:
+                        # cv2.imshow('orig', image_resize(orig, width=300))
+                        # cv2.imshow('other1', image_resize(others_images[others[0]], width=300))
+                        # cv2.imshow('other2', image_resize(others_images[others[1]], width=300))
+                        # orig = np.uint8(orig)
+                        # others_images[others[0]] = np.uint8(others_images[others[0]])
+                        # others_images[others[1]] = np.uint8(others_images[others[1]])
+                        if min_len_key == "R" and others[0] == "G":
+                            image = cv2.merge((others_images[others[1]], others_images[others[0]], orig))
+                        elif min_len_key == "R" and others[0] == "B":
+                            image = cv2.merge((others_images[others[0]], others_images[others[1]], orig))
+                        elif min_len_key == "G" and others[0] == "R":
+                            image = cv2.merge((others_images[others[1]], orig, others_images[others[0]]))
+                        elif min_len_key == "G" and others[0] == "B":
+                            image = cv2.merge((others_images[others[0]], orig, others_images[others[1]]))
+                        elif min_len_key == "B" and others[0] == "R":
+                            image = cv2.merge((orig, others_images[others[1]], others_images[others[0]]))
+                        elif min_len_key == "B" and others[0] == "G":
+                            image = cv2.merge((orig, others_images[others[0]], others_images[others[1]]))
+                        cv2.imwrite(os.path.join(IMG_PATH, i), image)
+                        # cv2.waitKey(0)
+                    
