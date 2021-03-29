f73b4275a1ea909dbc5843a68c7f72130d0135a9
MagicWinnie <magicwinnie@yandex.ru>
2021-03-30 00:52:12 +0700

    Now paralleling the download

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 - ### As the same images but with different channels do not have similar filenames, the script uses scikit-image's `compare_ssim` which might not correctly find the correct images to merge with.  
 - ### `combine.py` doesn't work. WIP  
 ## Requirements  
+`python>=3.7`  
 `pip3 install -r requirements.txt`  
 ## How to run  
 `python3 parse.py`  
--- parse.py
+++ parse.py
@@ -4,32 +4,16 @@ import shutil
 import requests
 import argparse
 import platform
+from typing import *
 import urllib.request
 from tqdm import tqdm
 from pathlib import Path
-
-def process_url(s: str)-> str:
-    ind_under = s.rindex('_')
-    ind_dot = s.rindex('.')
-    s = s[:ind_under] + s[ind_dot:]
-    s = s.replace('jpg', 'png')
-    return s
-
-def download_picture(url: str, path: str)->bool:
-    r = requests.get(url, stream = True)
-
-    if r.status_code == 200:
-        r.raw.decode_content = True
-        
-        with open(path, 'wb') as f:
-            shutil.copyfileobj(r.raw, f)
-            
-        return True
-    return False
+from parfive import Downloader
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--sol", type=int, default=-1, help="The sol you want to download (use -1 for all)")
 parser.add_argument("-r", "--rewrite", type=bool, default=False, help="Whether to rewrite existing files or not")
+parser.add_argument("-c", "--color", type=int, default=0, help="Whether to download color-processed images. Assuming it is encoded with 'F'. (0: download all, 1: download only color-processed, 2: download only raw")
 parser.add_argument("-a", "--api", type=str, required=True, help="Path to the file containing NASA API")
 args = parser.parse_args()
 
@@ -44,6 +28,8 @@ with open(args.api, 'r') as f:
 URL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/photos?sol={SOL}&api_key={API}'
 MANIFEST = 'https://api.nasa.gov/mars-photos/api/v1/manifests/perseverance/?api_key={API}'
 
+dl = Downloader()
+
 print("[INFO] GETTING DATA ABOUT ROVER")
 M_RESPONSE = requests.get(url=MANIFEST.format(API=API)).json()
 SOLS = [x["sol"] for x in M_RESPONSE['photo_manifest']['photos']]
@@ -53,19 +39,24 @@ if ((args.sol) != -1 and (args.sol not in SOLS)) or (args.sol < -1):
     raise ValueError("Sol number incorrect.\nAvailable: {}".format(SOLS))
 
 print("[INFO] STARTED SCRAPING")
-
 for i in tqdm(range(len(SOLS))):
     s = SOLS[i]
     resp = requests.get(url=URL.format(SOL=s, API=API)).json()
     Path(os.path.join(SAVE_PATH, str(s))).mkdir(parents=True, exist_ok=True)
     EXISTING = os.listdir(os.path.join(SAVE_PATH, str(s)))
-    for p in tqdm(range(PHOTOS_INFO[i]['total_photos']), leave=False):
-        DOWNLOAD_URL = resp["photos"][p]["img_src"]
-        DOWNLOAD_URL = DOWNLOAD_URL.replace('_1200.jpg', '.png')
-        if not(args.rewrite) and DOWNLOAD_URL.split('/')[-1] in EXISTING:
-            continue
-        status = download_picture(DOWNLOAD_URL, os.path.join(SAVE_PATH, str(s), DOWNLOAD_URL.split('/')[-1]))
-        if not(status):
-            print("Failed: URL: {}; SOL: {}; #: {}".format(DOWNLOAD_URL, s, p))
+    for p in range(PHOTOS_INFO[i]['total_photos']):
+        url = resp["photos"][p]["img_src"].replace('_1200.jpg', '.png')
+        path = os.path.join(SAVE_PATH, str(s))
+        if args.color == 1:
+            if url.split('/')[-1].split('_')[0][-1] == 'F':
+                dl.enqueue_file(url, path=path)
+        elif args.color == 2:
+            if url.split('/')[-1].split('_')[0][-1] != 'F':
+                dl.enqueue_file(url, path=path)
+        else:
+            dl.enqueue_file(url, path=path)
+print("[INFO] FINISHED SCRAPING")
 
+print("[INFO] STARTED DOWNLOADING")
+dl.download()
 print("[INFO] FINISHED DOWNLOADING")
